307081da319e206f82c1f99d9121d9442704814f
omelhoro <igor.fischer154@gmail.com>
2015-09-07 00:30:20 +0200

    some beautifying

--- flask_app.py
+++ flask_app.py
@@ -1,7 +1,8 @@
-from flask import Flask,request,redirect,send_from_directory
+from flask import Flask, request, redirect, send_from_directory
 from flask import render_template
 import re
 app = Flask(__name__)
+
 
 @app.route('/robots.txt')
 @app.route('/sitemap.xml')
@@ -9,53 +10,54 @@ def static_from_root():
     return send_from_directory(app.static_folder, request.path[1:])
 
 from rustress.webapi_blue import setup_rs
-app_rs=setup_rs()
+app_rs = setup_rs()
 app.register_blueprint(app_rs)
 
 # @app.route("/")
 # def index():
 #     return render_template("base.html")
-pages=(
-       ("/",("about_me","About me")),
-    ("/resumee",("","Resumee")),
-    ("/projects/<pro>",( 
-                        (
-                         ("sylls","Syllables (BA-App)"),
-                         ("syllsviz","LingViz"),
-                         ("slovdesc","Slovene"),
-                         ("praatxml","Praat & xml"),
-                         ("rustress","Russian stress"),
-                         ("foreignrus","Claiming Respect"),
-                         )
-                        ,"Projects")),
-    ("/sempapers",("","Study")),
-    ("/workviz",("","Work samples")), 
-       )
+pages = (
+    ("/", ("about_me", "About me")),
+    ("/resumee", ("", "Resumee")),
+    ("/projects/<pro>", (
+        (
+            ("sylls", "Syllables (BA-App)"),
+            ("syllsviz", "LingViz"),
+            ("slovdesc", "Slovene"),
+            ("praatxml", "Praat & xml"),
+            ("rustress", "Russian stress"),
+            ("foreignrus", "Claiming Respect"),
+        ), "Projects")),
+    ("/sempapers", ("", "Study")),
+    ("/workviz", ("", "Work samples")),
+)
+
 
 def create_route(routview):
-    
+
     def url(viewpath):
         def level(pro=None):
             if pro is None:
-                return render_template(viewpath+".html",pages=pages,path=request)
+                return render_template(viewpath + ".html", pages=pages, path=request)
             else:
-                return render_template(re.sub("<.+>","",viewpath)+pro+".html",pages=pages,path=request)
-        return level 
- 
-    route,(view,txt)=routview
-    if isinstance(view,tuple) or not view:
-        view=route 
-    f=url(view)  
-    f.__name__=view
+                return render_template(re.sub("<.+>", "", viewpath) + pro + ".html", pages=pages, path=request)
+        return level
+
+    route, (view, txt) = routview
+    if isinstance(view, tuple) or not view:
+        view = route
+    f = url(view)
+    f.__name__ = view
     return app.route(route)(f)
 
 
-#redirection for dart app, dart doest support relative links, but hard links then need symlinks, so its easier to simple redirect
+# redirection for dart app, dart doest support relative links, but hard
+# links then need symlinks, so its easier to simple redirect
 @app.route("/static/css/selfpoly.css")
 def redir():
     return redirect("/static/slov-apps/build/web/static/css/selfpoly.css")
-fns=list(map(create_route,pages) )
-        
-if __name__ == "__main__": 
+fns = list(map(create_route, pages))
+
+if __name__ == "__main__":
     app.debug = True
     app.run()
